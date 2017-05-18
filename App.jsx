d07f0a140c91c04c6590208c25b334c503904351
pbialy <pbialy@pbialy.eu>
2017-05-18 10:33:06 +0200

    initial commit (showing 1 img works)

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Page from './dev/js/components/Page.jsx'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div id={"mainDiv"}>
+                <img id={'firstImg'} style={{width:'200px', height:'150px'}} />
+            </div>
+        );
+    }
+}
+
+export default App;
